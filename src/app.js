a6072d2c64f4855a0d67b88c3c7788c5849976e3
yicheng <yichengzhang023@gmail.com>
2019-03-21 18:43:41 +1100

    create templates and header

--- src/app.js
+++ src/app.js
@@ -1,34 +1,50 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 
-app.set('view engine','hbs')
-app.use(express.static(path.join(__dirname,'../public')))
+const viewPath = path.join(__dirname, '../views/templates')
+const partialsPath = path.join(__dirname, '../views/partials')
+
+app.set('view engine', 'hbs')
+app.set('views', viewPath)
+hbs.registerPartials(partialsPath)
+
+app.use(express.static(path.join(__dirname, '../public')))
 
 // app.get('', (req, res) => {
 //     res.send('Hello, Express')
 // }) //route to homepage
 
 app.get('', (req, res) => {
-    res.render('index',{
-        title:'Weather',
-        name:'ande'
+    res.render('index', {
+        title: 'Weather',
+        name: 'ande'
     })
 }) // route to help page
 
 app.get('/help', (req, res) => {
-    res.send({title:'yes',object:'true'})
+    res.render('help', {
+        title: 'help',
+        name: 'yicheng'
+    })
 }) // route to help page
 
 app.get('/about', (req, res) => {
-    res.send('This is a about page')
+    res.render('about', {
+        title: 'about',
+        name: 'andrew'
+    })
 }) // route to help page
 
 app.get('/weather', (req, res) => {
     res.send('This is a weather page')
 }) // route to help page
 
+app.get('*', (req, res) => {
+    res.send('this is my 404 pages')
+})
 app.listen(3000, () => {
     console.log('Start system!')
 })
