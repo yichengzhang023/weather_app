9a89e1a40f204114e5fd37f603000cac1ddb1f81
yicheng <yichengzhang023@gmail.com>
2019-03-24 17:49:58 +1100

    new

--- src/app.js
+++ src/app.js
@@ -5,16 +5,21 @@ const gecode = require('./utils/geoCode')
 const forecast = require('./utils/forecast')
 
 const app = express()
-
-const viewPath = path.join(__dirname, '../views/templates')
-const partialsPath = path.join(__dirname, '../views/partials')
 const port = process.env.PORT || 3000
 
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine and views location
 app.set('view engine', 'hbs')
-app.set('views', viewPath)
+app.set('views', viewsPath)
 hbs.registerPartials(partialsPath)
 
-app.use(express.static(path.join(__dirname, '../public')))
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
 
 // app.get('', (req, res) => {
 //     res.send('Hello, Express')
