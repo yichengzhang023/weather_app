dfcdbb5949258ae7ecd81e26b7539d2602f29195
yicheng <yichengzhang023@gmail.com>
2019-03-24 17:54:10 +1100

    new

--- src/app.js
+++ src/app.js
@@ -9,8 +9,8 @@ const port = process.env.PORT || 3000
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates/views')
-const partialsPath = path.join(__dirname, '../templates/partials')
+const viewsPath = path.join(__dirname, '../views/templates')
+const partialsPath = path.join(__dirname, '../views/partials')
 
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs')
